148a18bf0d925f58c52465faab4971289b28f511
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-01-09 11:26:43 -0300

    Project created

---
+++ src/test/java/com/project/financialmanagement/FinancialmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.financialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
