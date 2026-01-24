52539eb2f45c3b06c3c0ddc7d1addc95681164fe
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-01-24 09:53:33 -0300

    Insert default categories

---
+++ src/main/resources/db/migration/V5__insert_default_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories (name, type) VALUES
+("Salário", "INCOME"),
+("Freelance", "INCOME"),
+("Alimentação", "EXPENSE"),
+("Transporte", "EXPENSE"),
+("Cartão de crédito", "EXPENSE"),
+("Aluguel", "EXPENSE");
+
+
